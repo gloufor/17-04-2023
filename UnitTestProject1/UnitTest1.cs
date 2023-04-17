82864d2d069653960fe43d425fdd12a3bf45ac26
gloufor <94900586+gloufor@users.noreply.github.com>
2023-04-17 12:54:14 +0700

    14.04.2023

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Name_WithLessThan3Characters_ReturnFalse()
+        {
+            string name = "ab";
+            bool result = IsNameValid(name);
+            Assert.IsFalse(result);
+        }
+
+        public void Name_WithMoreThan15Characters_ReturnFalse()
+        {
+            string name = "asdfrghbnhgfcvgf";
+            bool result = IsNameValid(name);
+            Assert.IsFalse(result);
+        }
+
+        public void Name_With3To15Characters_ReturnsTrue()
+        {
+            string name1 = "abc";
+            string name2 = "asdfrghbnhgfcvf";
+
+            bool result1 = IsNameValid(name1);
+            bool result2 = IsNameValid(name2);
+
+            Assert.IsTrue(result1);
+            Assert.IsTrue(result2);
+            
+        }
+        private bool IsNameValid(string name)
+        {
+            if (name.Length < 10 || name.Length > 50)
+            {
+                return false;
+            }
+            return false;
+        }
+        
+    }
+}
---
+++ ekzamen/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ekzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void imya_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(imyaBox.Text.Length < 3 || imyaBox.Text.Length > 15)
+            {
+                imyaBox.Background = Brushes.Red;
+                string text = "Отказано!";
+                imyaBlock.Text = text;
+            }
+            else
+            {
+                imyaBox.Background = Brushes.Green;
+                string text1 = "Одобрено!";
+                imyaBlock.Text = text1;
+            }
+        }
+
+        private void familiyaBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (familiyaBox.Text.Length < 2 || familiyaBox.Text.Length > 25)
+            {
+                familiyaBox.Background = Brushes.Red;
+                string text = "Отказано!";
+                familiyaBlock.Text = text;
+            }
+            else
+            {
+                familiyaBox.Background = Brushes.Green;
+                string text1 = "Одобрено!";
+                familiyaBlock.Text = text1;
+            }
+
+        }
+    }
+    
+}
